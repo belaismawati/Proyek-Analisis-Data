808f3add428c6445a35af74cc7d6d7ddd9e23776
Bela Ismawati Nuraisa <belaismawati292@gmail.com>
2025-03-15 00:28:56 +0700

    Update Dashboard.py

--- dashboard/Dashboard.py
+++ dashboard/Dashboard.py
@@ -45,7 +45,7 @@ def create_seller_bystate(df):
     return seller_bystate
 
 #  load berkas analisis_data.csv
-all_df = pd.read_excel(dashboard/analisis_data.xls)
+all_df = pd.read_excel("analisis_data.xls")
 
 datetime_columns = ["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", 
 "order_estimated_delivery_date", "shipping_limit_date", "review_creation_date", "review_answer_timestamp"]
